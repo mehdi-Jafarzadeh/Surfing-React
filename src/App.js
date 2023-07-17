8bdfe47e48091d80d471dd21c18dcf5ae84c2fff
mehdi <mehdijafarzadehh4@gmail.com>
2023-07-17 11:55:14 +0330

    finished

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import P4 from './components/P4/P4';
 import Navbar from './components/Navbar/Navbar';
 import P2 from './components/P2/P2';
 import P3 from './components/P3/P3';
-// import Sidebar from './components/Sidebar/Sidebar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Sidebar /> */}
+      <Sidebar />
       <P1 />
       <P2 />
       <P3 />
